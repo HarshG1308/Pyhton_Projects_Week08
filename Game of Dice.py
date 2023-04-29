e5ce660f573fe8cc3a5a0e9447f345e3f739d182
Harsh Gautam <114353823+HarshG1308@users.noreply.github.com>
2023-04-30 00:29:05 +0530

    Update Game of Dice.py

--- Game of Dice.py
+++ Game of Dice.py
@@ -2,24 +2,24 @@ import random
 
 print('\n\n----------------------Welcome to Game of Dice----------------------\n')
 name = input('Enter your name: ')
-print(f"\nHello! {name} Let's Roll !!..")
-ch = 'Yes'
-list = []
-while ch == 'Yes' or ch == 'yes':
+print(f"\nHello! {name} Let's Start !!..")
+ch = 'Y'
+l = []
+while ch == 'Y' or ch == 'y':
     print('\n1. Roll\n2. Exit')
-    n = int(input('\nEnter Your Choice: '))
-    out1= random.choice([1,2,3,4,5,6])
-    out2= random.choice([1,2,3,4,5,6])
-    if n == 1:
-        print('\nNumber on the dices are: ',out1,out2)
-        list.append([out1,out2])
-    elif n == 2:
-        print('\nTotal outcomes: ',list)
+    a = int(input('\nEnter Your Choice: '))
+    c1= random.choice([1,2,3,4,5,6])
+    c2= random.choice([1,2,3,4,5,6])
+    if a == 1:
+        print('\nNumber on the dices are: ',c1,c2)
+        l.append([c1,c2])
+    elif a == 2:
+        print('\nTotal outcomes: ',l)
         print('\n\t\tThank You')
     else:
         print('\nEnter valid choice')
-        n = int(input('\nEnter Your Choice: '))
-    ch = input("\nDo you want to continue(Yes/No): ")
+        a = int(input('\nEnter Your Choice: '))
+    ch = input("\nDo you want to continue(Y/N): ")
 else:
-    print('\nTotal outcomes: ',list)
+    print('\nTotal outcomes: ',l)
     print('\n\t\t...Thank You for Playing...')
