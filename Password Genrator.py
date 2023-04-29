f0c7fddf968eb1ba244f39c1e84d2206ee21edbc
Harsh Gautam <114353823+HarshG1308@users.noreply.github.com>
2023-04-30 00:30:15 +0530

    Update Password Genrator.py

--- Password Genrator.py
+++ Password Genrator.py
@@ -3,32 +3,32 @@ import string
 
 print('\n\n----------------------Welcome to Strong Password Generator----------------------\n')
 
-u = string.ascii_uppercase
-l = string.ascii_lowercase
-d = string.digits
-p = string.punctuation
+up = string.ascii_uppercase
+lo = string.ascii_lowercase
+di = string.digits
+pu = string.punctuation
 
 pw = ''
-a = int(input('\n How many characters you want in password (8/12):'))
+n = int(input('\n How many characters you want in password (8/12):'))
 
-if a == 8:
-    u1 = random.choices(u,k=2)
-    l1 = random.choices(l,k=2)
-    d1 = random.choices(d,k=2)
-    p1 = random.choices(p,k=2)
-    pwl = u1 + l1 + d1 + p1
+if n == 8:
+    up1 = random.choices(up,k=2)
+    lo1 = random.choices(lo,k=2)
+    di1 = random.choices(di,k=2)
+    pu1 = random.choices(pu,k=2)
+    pwl = up1 + lo1 + di1 + pu1
     for i in pwl:
         pw = pw + i
-elif a==12:
-    u1 = random.choices(u,k=3)
-    l1 = random.choices(l,k=3)
-    d1 = random.choices(d,k=3)
-    p1 = random.choices(p,k=3)
-    pwl = u1 + l1 + d1 + p1
+elif n==12:
+    up1 = random.choices(up,k=3)
+    lo1 = random.choices(lo,k=3)
+    di1 = random.choices(di,k=3)
+    pu1 = random.choices(pu,k=3)
+    pwl = up1 + lo1 + di1 + pu1
     for i in pwl:
         pw = pw + i
 else:
     print('Enter valid choice...')
-    a = int(input('\n How many characters you want in password (8/12): '))
+    n = int(input('\n How many characters you want in password (8/12): '))
 
 print('\n\t\t Generated password is: ',pw)
