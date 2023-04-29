ba7a716dadf86c4b8d3d54f5a763855e890bf231
Harsh Gautam <114353823+HarshG1308@users.noreply.github.com>
2023-04-30 00:28:32 +0530

    Update OTP Generator.py

--- OTP Generator.py
+++ OTP Generator.py
@@ -1,11 +1,29 @@
+# generate a 6-digit OTP
 import random
+import string
 
 print('\n\n----------------------OTP Generator----------------------\n\n')
 
-o = random.randint(1000,9999)
+otp = ""
+for i in range(6):
+    otp += str(random.randint(0, 9))
 
-print("\t\tYour 4 digit OTP is:",o)
+print("\t\tYour 6 digit OTP is:", otp)
 
-o = random.randint(100000,999999)
+# generate a 4-digit OTP
+otp = ""
+for i in range(4):
+    otp += str(random.randint(0, 9))
 
-print("\n\t\tYour 4 digit OTP is:",o)
+print("\n\t\tYour 4 digit OTP is:", otp)
+
+#generate a 6-digit alphanumeric OTP
+otp = ""
+d = string.digits
+l = string.ascii_letters
+d1 = random.choices(d,k=3)
+l2 = random.choices(l,k=3)
+f = d1 + l2
+for i in f :
+    otp += i
+print("\n\t\tYour 6 digit alphanumeric OTP is:", otp)
