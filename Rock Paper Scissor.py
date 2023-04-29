75268aafc6141a1b5c9272a35d1ddc66c6e4ddc7
Harsh Gautam <114353823+HarshG1308@users.noreply.github.com>
2023-04-30 00:28:10 +0530

    Update Rock Paper Scissor.py

--- Rock Paper Scissor.py
+++ Rock Paper Scissor.py
@@ -1,16 +1,16 @@
 import random 
 
-print('\n\n----------------------Welcome to Game of Rock Paper and Scissor----------------------\n\n')
+print('\n\n----------------------Welcome to Game of Rock Paper and Scissor (Computer Vs Human)----------------------\n\n')
 
-a = 'Y'
+ch = 'Y'
 
-while a == 'Y' or a == 'y':
+while ch == 'Y' or ch == 'y':
     c1 = random.choice(['Rock','Paper','Scissor'])
     c2 = input('\nEnter your choice: ')
     if (c1 == 'Rock' and c2 == 'Scissor') or (c1 == 'Paper' and c2 == "Rock") or (c1 == 'Scissor' and c2 == 'Paper'):
-        print('\n              Computer Wins... !!')
+        print('\n              Computer Wins !!')
     elif c1 == c2:
-        print("\n              It is a Tie Game...")
+        print("\n              It's a Tie")
     else:
-        print('\n              You Wins...')
-    a = input("\nDo you want to continue(Y/N): ")
+        print('\n              Congratulations !! You Win...')
+    ch = input("\nDo you want to continue(Y/N): ")
