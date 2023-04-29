7937aae0acbf561432324a4c25719f13ec4072b5
Harsh Gautam <114353823+HarshG1308@users.noreply.github.com>
2023-04-30 00:05:25 +0530

    Add files via upload
    OTP generator via python...

---
+++ OTP Generator.py
@@ -0,0 +1,11 @@
+import random
+
+print('\n\n----------------------OTP Generator----------------------\n\n')
+
+o = random.randint(1000,9999)
+
+print("\t\tYour 4 digit OTP is:",o)
+
+o = random.randint(100000,999999)
+
+print("\n\t\tYour 4 digit OTP is:",o)
